aa2ae1eef4412370b3935107b7af049ef08fd52c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-11 18:02:50 +0900

    copy files from Memorize

---
+++ MineSweeper/Array+Identifiable.swift
@@ -0,0 +1,20 @@
+//
+//  Array+Identifiable.swift
+//  MemoryGame
+//
+//  Created by 楢崎修二 on 2020/10/03.
+//
+
+import Foundation
+
+extension Array where Element: Identifiable {
+    func firstIndex(matching: Element) -> Int? {
+        for index in 0..<self.count {
+            if self[index].id == matching.id {
+                return index
+            }
+        }
+        return nil
+    }
+}
+
